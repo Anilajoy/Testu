0c32c81c438ac415a0defc83ffbbc61fbf5587e8
Anilajoy <elzaanila2004@gmail.com>
2021-02-26 14:44:19 +0200

    last

---
+++ jiby.java
@@ -0,0 +1,17 @@
+class jiby
+{
+   public static void main(String[] args) {
+       System.out.println("Hello");
+       int num=0;
+       int c=0;
+       //dfdf
+       //String
+       //dfdfdffdsfdfdfdfsdfdf
+       //dffg
+       //fgf
+       //fgfg
+       
+
+   }
+
+}
